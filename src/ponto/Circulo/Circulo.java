4fce8996b4015cdb113574f9545680a5b2a4a17d
BNovo04 <brunonovo267@gmail.com>
2023-09-04 20:35:37 -0300

    Ajustes e comentarios dos arquivos do circulo.

--- src/ponto/Circulo/Circulo.java
+++ src/ponto/Circulo/Circulo.java
@@ -7,11 +7,26 @@ public class Circulo {
     private Ponto ponto2;
     private int raio;
 
+    // Construtores
+    /**
+     * CirculoGr - Constroi uma circulo com valores p e diametro
+     *
+     * @param p int. Coordenada ponto inicial
+     * @param diametro int. Diametro da circunferencia
+     */
     public Circulo(Ponto p, int diametro){
         this.ponto1 = p;
         this.raio = diametro / 2;
     }
 
+    /**
+     * Constroi um circulo com valores de x1, y1 e x2, y2
+     *
+     * @param x1 coordenada x de p1
+     * @param y1 coordenada y de p1
+     * @param x2 coordenada x de p2
+     * @param y2 coordenada y de p2
+     */
     public Circulo(int x1, int y1, int x2, int y2){
         this.ponto1 = new Ponto(x1, y1);
         this.ponto2 = new Ponto(x2, y2);
@@ -19,12 +34,22 @@ public class Circulo {
         if(this.raio < 0) this.raio = this.raio * -1;
     }
 
+
+    /**
+     * Constroi um circulo com valores de x1, y1 e x2, y2
+     *
+     * @param x1 coordenada x de p1
+     * @param y1 coordenada y de p1
+     * @param x2 coordenada x de p2
+     * @param y2 coordenada y de p2
+     */
     public Circulo(double x1, double y1, double x2, double y2){
         this.ponto1 = new Ponto((int) x1, (int) y1);
         this.ponto2 = new Ponto( (int) x2, (int) y2);
         this.raio = (int) Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 - y2),2));
     }
 
+    //getters e setters
     public Ponto getPonto1() {
         return ponto1;
     }
--- src/ponto/Circulo/CirculoGr.java
+++ src/ponto/Circulo/CirculoGr.java
@@ -9,31 +9,64 @@ public class CirculoGr extends Circulo{
 
     private String nome;
     private int espessura;
-    private Color corAtual = Color.BLUE;
-
+    private Color corAtual;
+    // Construtores
+    /**
+     * CirculoGr - Constroi uma circulo grafico
+     *
+     * @param p int. Coordenada ponto inicial
+     * @param diametro int. Diametro da circunferencia
+     */
     public CirculoGr(Ponto p, int diametro) {
         super(p, diametro);
     }
 
+    /**
+     * CirculoGr - Constroi um circulo grafico
+     *
+     * @param x1 int. Coordenada xCentral
+     * @param y1 int. Coordenada yCentral
+     * @param x2 int. Coordenada xBorda
+     * @param y2 int. Coordenada yBorda
+     * @param cor Color. Cor do circulo
+     * @param nome String. Nome do circulo
+     * @param esp int. Espessura do circulo
+     */
     public CirculoGr(int x1, int y1, int x2, int y2, String nome, int espessura, Color corAtual){
         super(x1,y1,x2,y2);
         this.nome = nome;
         this.espessura = espessura;
-//        this.corAtual = corAtual;
+        this.corAtual = corAtual;
     }
-
+    /**
+     * CirculoGr - Constroi um circulo grafico
+     *
+     * @param x1 int. Coordenada xCentral
+     * @param y1 int. Coordenada yCentral
+     * @param x2 int. Coordenada xBorda
+     * @param y2 int. Coordenada yBorda
+     * @param cor Color. Cor do circulo
+     * @param nome String. Nome do circulo
+     * @param esp int. Espessura do circulo
+     */
     public CirculoGr(double x1, double y1, double x2, double y2, String nome, int espessura, Color corAtual){
         super(x1,y1,x2,y2);
         this.nome = nome;
         this.espessura = espessura;
-//        this.corAtual = corAtual;
+        this.corAtual = corAtual;
     }
-
+    /**
+     * Desenha circulo grafico com base nas equaçoes
+     * xBorda = xCentral + cosseno * raio
+     * yBorda = yCentral + seno * raio
+     *
+     * @param g Graphics. Classe com os metodos graficos do Java
+     */
     public void desenharCirculo(Graphics g){
         int xCentral = (int) getPonto1().getX();
         int yCentral = (int) getPonto1().getY();
-        double x = (int) getPonto2().getX();
-        double y = (int) getPonto2().getY();
+        double xBorda = (int) getPonto2().getX();
+        double yBorda = (int) getPonto2().getY();
         double raio = getRaio();
         int theta;
 
@@ -41,54 +74,10 @@ public class CirculoGr extends Circulo{
             double radiano = Math.toRadians(theta);
             double seno = Math.sin(radiano);
             double cosseno = Math.cos(radiano);
-            x = xCentral + cosseno * raio;
-            y = yCentral + seno * raio;
-            PontoGr circulo = new PontoGr((int) x,(int) y, corAtual, espessura);
+            xBorda = xCentral + cosseno * raio;
+            yBorda = yCentral + seno * raio;
+            PontoGr circulo = new PontoGr((int) xBorda,(int) yBorda, corAtual, espessura);
             circulo.desenharPonto(g);
         }
-
-/*        PontoGr p1 = new PontoGr((x + xCentral), (y + yCentral), Color.black, espessura);
-        p1.desenharPonto(g);
-        PontoGr p2 = new PontoGr((x + xCentral), ((-1 * y) + yCentral), Color.black, espessura);
-        p2.desenharPonto(g);
-        PontoGr p3 = new PontoGr((y + xCentral), (x + yCentral), Color.black, espessura);
-        p3.desenharPonto(g);
-        PontoGr p4 = new PontoGr(((-1 * y) + xCentral), (x + yCentral), Color.black, espessura);
-        p4.desenharPonto(g);
-
-        while(x > y){
-            y++;
-            if(p <= 0){
-                p = p + (2 * y) + 1;
-            }else{
-                x--;
-                p = p + (2 * y) - (2 * x) + 1;
-            }
-
-            if(x < y){
-                break;
-            }
-
-            PontoGr p5 = new PontoGr((x + xCentral), (y + yCentral), corAtual, espessura);
-            p5.desenharPonto(g);
-            PontoGr p6 = new PontoGr(( (-1 * x) + xCentral), (y + yCentral), corAtual, espessura);
-            p6.desenharPonto(g);
-            PontoGr p7 = new PontoGr((x + xCentral), ((-1 * y) + yCentral), corAtual, espessura);
-            p7.desenharPonto(g);
-            PontoGr p8 = new PontoGr(((-1 * x) + xCentral), ((-1 * y) + yCentral), corAtual, espessura);
-            p8.desenharPonto(g);
-
-            if( x != y){
-                PontoGr p9 = new PontoGr((y + xCentral), (x + yCentral), corAtual, espessura);
-                p9.desenharPonto(g);
-                PontoGr p10 = new PontoGr(( (-1 * y) + xCentral), (x + yCentral), corAtual, espessura);
-                p10.desenharPonto(g);
-                PontoGr p11 = new PontoGr((y + xCentral), ((-1 * x) + yCentral), corAtual, espessura);
-                p11.desenharPonto(g);
-                PontoGr p12 = new PontoGr(((-1 * y) + xCentral), ((-1 * x) + yCentral), corAtual, espessura);
-                p12.desenharPonto(g);
-            }
-        }
-    */
     }
 }
